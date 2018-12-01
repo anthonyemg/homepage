2bd4ce5f5a1c3ac590be714b0f496bac818c2156
Anthony Greenheck <aemgreenheck@gmail.com>
2018-12-01 18:56:20 -0500

    Added greeting text

--- src/public/app/container/App/index.js
+++ src/public/app/container/App/index.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className='app-container'>
-        {/* <img src='https://farm7.staticflickr.com/6154/6202343476_881f579ecf_o.jpg' /> */}
+        <span>hello</span>
       </div>
     )
   }

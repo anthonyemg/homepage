3ef3ae1a9061430b4c51182135d792e8a9aa0d7f
Anthony Greenheck <aemgreenheck@gmail.com>
2018-12-17 22:31:51 -0500

    Updated SideMenu transition and functionality

--- src/public/app/components/SideMenu/index.js
+++ src/public/app/components/SideMenu/index.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SideMenu extends Component {
   constructor(props) {
     super(props);
+
+    this.handleCheckIfTargetIsMain = this.handleCheckIfTargetIsMain.bind(this);
   }
 
   componentDidUpdate(prev) {
-    if (prev.displayMenu === false && this.props.displayMenu) {
+    if (!prev.displayMenu && this.props.displayMenu) {
       this.inputField.focus();
+      document.querySelector('.main').addEventListener('click', this.handleCheckIfTargetIsMain, true);
+    }
+
+    if (prev.displayMenu && !this.props.displayMenu) {
+      document.querySelector('.main').removeEventListener('click', this.handleCheckIfTargetIsMain, true);
     }
   }
 
+  handleCheckIfTargetIsMain(e) {
+    if (e.target.classList.contains('main')) {
+      this.props.handleToggleMenu();
+    }
+  }
 
   handleOnChange(e) {
     this.setState({ username: e.target.value })
@@ -42,46 +55,51 @@ class SideMenu extends Component {
   render() {
     const {
       displayMenu,
-      handleToggleMenu,
+      // handleToggleMenu,
       isSearchLoading,
       username,
       warningMessage,
     } = this.props;
 
     return (
-      <div
-        className="side-menu-container"
+      <div className="side-menu"
         data-display-side-menu={displayMenu}
-        onClick={() => handleToggleMenu()}
+        onClick={(e) => { e.stopPropagation; }}
       >
-        <div className="side-menu"
-        onClick={(e) => e.stopPropagation()}
+      <div
+        className="side-menu-navigation-search"
+        onClick={(e) => { e.stopPropagation; }}
+      >
+        {isSearchLoading &&
+        <i className="side-menu-navigation-search-loading fa fa-spinner fa-spin"></i>}
+
+        <input
+          onChange={e => this.handleOnChange(e)}
+          onKeyDown={e => this.handleOnEnterKeyDown(e)}
+          value={username}
+          ref={el => { this.inputField = el } }
+        />
+        <div
+          className="side-menu-navigation-search-button material-icons"
+          onClick={() =>  this.handleOnEnterClick(username)}
         >
-
-        <div className="side-menu-navigation-search">
-          {isSearchLoading &&
-          <i className="side-menu-navigation-search-loading fa fa-spinner fa-spin"></i>}
-
-          <input
-            onChange={e => this.handleOnChange(e)}
-            onKeyDown={e => this.handleOnEnterKeyDown(e)}
-            value={username}
-            ref={el => { this.inputField = el } }
-          />
-          <div
-            className="side-menu-navigation-search-button material-icons"
-            onClick={() =>  this.handleOnEnterClick(username)}
-          >
-            search
-          </div>
-        </div> 
-
-        {warningMessage !== '' && <div className="side-menu-navigation-search-warning">{warningMessage}</div>}
-
+          search
         </div>
+      </div> 
+
+      {warningMessage !== '' && <div className="side-menu-navigation-search-warning">{warningMessage}</div>}
+
       </div>
     )
   }
 }
+
+SideMenu.propTypes = {
+  displayMenu: PropTypes.bool,
+};
+
+SideMenu.defaultProps = {
+  displayMenu: false,
+};
 
 export default SideMenu;
